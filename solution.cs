b8f20b0f9982f094b5316b0339baf0e4b9d64aa9
Kooreis <kooreis.official@gmail.com>
2024-03-25 02:55:06 -0500

    Created the Account class with properties for Id and Balance. Also added a constructor to initialize these properties.

--- solution.cs
+++ solution.cs
@@ -1,4 +1,3 @@
-```C#
 using System;
 
 public class Account
@@ -11,54 +10,4 @@ public class Account
         Id = id;
         Balance = initialBalance;
     }
-
-    public void Deposit(decimal amount)
-    {
-        Balance += amount;
-    }
-
-    public void Withdraw(decimal amount)
-    {
-        if (Balance >= amount)
-        {
-            Balance -= amount;
-        }
-        else
-        {
-            Console.WriteLine("Insufficient balance");
-        }
-    }
-
-    public void Transfer(Account toAccount, decimal amount)
-    {
-        if (Balance >= amount)
-        {
-            Withdraw(amount);
-            toAccount.Deposit(amount);
-        }
-        else
-        {
-            Console.WriteLine("Insufficient balance");
-        }
-    }
 }
-
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        Account account1 = new Account("001", 500);
-        Account account2 = new Account("002", 300);
-
-        account1.Deposit(200);
-        Console.WriteLine("Account 1 balance: " + account1.Balance);
-
-        account1.Withdraw(100);
-        Console.WriteLine("Account 1 balance: " + account1.Balance);
-
-        account1.Transfer(account2, 200);
-        Console.WriteLine("Account 1 balance: " + account1.Balance);
-        Console.WriteLine("Account 2 balance: " + account2.Balance);
-    }
-}
-```
