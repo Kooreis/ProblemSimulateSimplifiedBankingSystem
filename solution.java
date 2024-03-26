e74a2fefb7feddabd3788307907f41df790a2641
Kooreis <kooreis.official@gmail.com>
2024-03-26 01:55:06 -0500

    Created an Account class with methods for depositing, withdrawing, and transferring funds, as well as checking the balance.

--- solution.java
+++ solution.java
@@ -1,1 +1,32 @@
-import java.util.Scanner;
+class Account {
+    private double balance;
+
+    public Account(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient balance");
+        }
+    }
+
+    public void transfer(Account to, double amount) {
+        if (balance >= amount) {
+            withdraw(amount);
+            to.deposit(amount);
+        } else {
+            System.out.println("Insufficient balance");
+        }
+    }
+}
